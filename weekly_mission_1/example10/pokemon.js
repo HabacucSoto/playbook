0a1996ad06270d3be20bf65d4827251f5f8bb802
habacuc <josehabacuc182020@gmail.com>
2022-04-21 16:25:28 -0500

    practices first week

---
+++ weekly_mission_1/example10/pokemon.js
@@ -0,0 +1,17 @@
+class MyPokemon {
+
+    constructor( name ){
+
+        this.name = name
+
+    }
+
+    sayHello(){
+
+        console.log(`Mi pokemon ${ this.name } te saluda!`)
+
+    }
+
+}
+
+export default MyPokemon
